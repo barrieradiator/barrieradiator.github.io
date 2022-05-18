2e9bcb532cd51b1ee1ffad27757ac6ce801f93f0
Kyle James <kyle.james1@dcmail.ca>
2022-05-17 20:17:49 -0400

    javascript routing complete

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const WEBSITE_NAME = "Barrie Radiator";
 
-
-	
 function displayIndex() {
+	document.title = WEBSITE_NAME;
 	var title = document.getElementById("header");
 	var paragraph = document.getElementById("main-body-paragraph");
 	
@@ -11,34 +10,73 @@ function displayIndex() {
 }
 
 function displayHome() {
+	var pageTitle = " - Home";
+	document.title = WEBSITE_NAME + pageTitle;
 	var title = document.getElementById("header");
 	var paragraph = document.getElementById("main-body-paragraph");
 	
-	title.innerHTML = WEBSITE_NAME + " - Home";
+	title.innerHTML = WEBSITE_NAME + pageTitle;
 	paragraph.innerHTML = "Home";
 }
 
 function displayAboutUs() {
+	var pageTitle = " - About Us";
+	document.title = WEBSITE_NAME + pageTitle;
 	var title = document.getElementById("header");
 	var paragraph = document.getElementById("main-body-paragraph");
 	
-	title.innerHTML = WEBSITE_NAME + " - About Us";
+	title.innerHTML = WEBSITE_NAME + pageTitle;
 	paragraph.innerHTML = "Gerry is one of 2 licensed mechanics! and other text data goes here. Images can go here also.";
 }
 
 function displayContactUs() {
+	var pageTitle = " - Contact Us";
+	document.title = WEBSITE_NAME + pageTitle;
 	var title = document.getElementById("header");
 	var paragraph = document.getElementById("main-body-paragraph");
 	
-	title.innerHTML = WEBSITE_NAME + " - Contact Us";
-	paragraph.innerHTML = "Contact Us: <br>Email:<a href='mailto:mail@mail.com'>mail@mail.com</a> <br>Phone: <a href='phone:9051234567'>(905)123-4567</a>";
+	title.innerHTML = WEBSITE_NAME + pageTitle;
+	paragraph.innerHTML = "Contact Us: <br>Email:<a href='mailto:mail@mail.com'>mail@mail.com</a> <br>Phone: <a href='tel:9051234567'>(905)123-4567</a>";
 }
 
 function displayFindUs() {
+	var pageTitle = " - Find Us";
+	document.title = WEBSITE_NAME + pageTitle;
 	var title = document.getElementById("header");
 	var paragraph = document.getElementById("main-body-paragraph");
 	
-	title.innerHTML = WEBSITE_NAME + " - Find Us";
+	title.innerHTML = WEBSITE_NAME + pageTitle;
 	paragraph.innerHTML = "You can find us at 123 main street";
 	
+}
+
+function displayError() {
+	var pageTitle = " - Error";
+	document.title = WEBSITE_NAME + pageTitle;
+	var title = document.getElementById("header");
+	var paragraph = document.getElementById("main-body-paragraph");
+	
+	title.innerHTML = WEBSITE_NAME + pageTitle;
+	paragraph.innerHTML = "Error! Something went wrong!";
+}
+
+function checkUrlAndLoadCorrespondingDisplay() {
+	if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html#home" || window.location.href == "/index.html#home" || window.location.href == "/#home") {
+		displayHome();
+	}
+	else if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html" || window.location.href == "/index.html" || window.location.href == "/") {
+		displayIndex();
+	}
+	else if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html#about-us" || window.location.href == "/index.html#about-us" || window.location.href == "/#about-us") {
+		displayAboutUs();
+	}
+	else if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html#contact-us" || window.location.href == "/index.html#contact-us" || window.location.href == "/#contact-us") {
+		displayContactUs();
+	}
+	else if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html#find-us" || window.location.href == "/index.html#find-us" || window.location.href == "/#find-us") {
+		displayFindUs();
+	}
+	else {
+		displayError();
+	}
 }
