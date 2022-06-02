59ab6ad55fe3309d560221549926559102cb060b
Kyle James <kyle.james1@dcmail.ca>
2022-06-02 17:33:57 -0400

    made pages more seo friendly

--- script.js
+++ script.js
@@ -1,74 +1,70 @@
 const WEBSITE_NAME = "Barrie Radiator";
 
+const INDEX_HTML_PATH = "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html"
+
+function hideAllPages() {	
+	var page_index = document.getElementById("page-index");
+	page_index.setAttribute("hidden", "hidden");
+	var page_about_us = document.getElementById("page-about-us");
+	page_about_us.setAttribute("hidden", "hidden"); 
+	var page_contact_us = document.getElementById("page-contact-us");
+	page_contact_us.setAttribute("hidden", "hidden"); 
+	var page_find_us = document.getElementById("page-find-us");
+	page_find_us.setAttribute("hidden", "hidden"); 
+	var page_services = document.getElementById("page-services");
+	page_services.setAttribute("hidden", "hidden"); 
+	var page_current_sales = document.getElementById("page-current-sales");
+	page_current_sales.setAttribute("hidden", "hidden"); 
+}
+
 function displayIndex() {
+	hideAllPages();
 	document.title = WEBSITE_NAME;
-	var title = document.getElementById("header");
-	var paragraph = document.getElementById("main-body-paragraph");
-	
-	title.innerHTML = WEBSITE_NAME;
-	paragraph.innerHTML = "Default Home Page Data";
+	var current_page = document.getElementById("page-index");
+	current_page.removeAttribute("hidden"); 
 }
 
 function displayHome() {
-	var pageTitle = "Home";
-	document.title = WEBSITE_NAME + " - " + pageTitle;
-	var title = document.getElementById("header");
-	var paragraph = document.getElementById("main-body-paragraph");
-	
-	title.innerHTML = pageTitle;
-	paragraph.innerHTML = "Home";
+	hideAllPages();
+	document.title = WEBSITE_NAME + " - Home";
+	var current_page = document.getElementById("page-home");
+	current_page.removeAttribute("hidden"); 
 }
 
 function displayAboutUs() {
-	var pageTitle = "About Us";
-	document.title = WEBSITE_NAME + " - " + pageTitle;
-	var title = document.getElementById("header");
-	var paragraph = document.getElementById("main-body-paragraph");
-	
-	title.innerHTML = pageTitle;
-	paragraph.innerHTML = "Gerry is one of 2 licensed mechanics! and other text data goes here. Images can go here also.";
+	hideAllPages();
+	document.title = WEBSITE_NAME + " - About Us";
+	var current_page = document.getElementById("page-about-us");
+	current_page.removeAttribute("hidden"); 
 }
 
 function displayContactUs() {
-	var pageTitle = "Contact Us";
-	document.title = WEBSITE_NAME + " - " + pageTitle;
-	var title = document.getElementById("header");
-	var paragraph = document.getElementById("main-body-paragraph");
-	
-	title.innerHTML = pageTitle;
-	paragraph.innerHTML = "Contact Us: <br>Email: <a href='mailto:mail@mail.com'>mail@mail.com</a> <br>Phone: <a href='tel:9051234567'>(905)123-4567</a>";
+	hideAllPages();
+	document.title = WEBSITE_NAME + " - Contact Us";
+	var current_page = document.getElementById("page-contact-us");
+	current_page.removeAttribute("hidden"); 
 }
 
 function displayFindUs() {
-	var pageTitle = "Find Us";
-	document.title = WEBSITE_NAME + " - " + pageTitle;
-	var title = document.getElementById("header");
-	var paragraph = document.getElementById("main-body-paragraph");
-	
-	title.innerHTML = pageTitle;
-	paragraph.innerHTML = "You can find us at 123 main street!<br>(If you wish, we could add google maps to this page.)";
-	
+	hideAllPages();
+	document.title = WEBSITE_NAME + " - Find Us";	
+	var current_page = document.getElementById("page-find-us");
+	current_page.removeAttribute("hidden"); 
+
 }
 
 function displayServices() {
-	var pageTitle = "Services";
-	document.title = WEBSITE_NAME + " - " + pageTitle;
-	var title = document.getElementById("header");
-	var paragraph = document.getElementById("main-body-paragraph");
-	
-	title.innerHTML = pageTitle;
-	paragraph.innerHTML = "Our services are as follows:<br>(This could be an image, a PDF, or just a text menu.)";
-	
+	hideAllPages();
+	document.title = WEBSITE_NAME + " - Services";	
+	var current_page = document.getElementById("page-services");
+	current_page.removeAttribute("hidden"); 
 }
 
 function displayCurrentSales() {
-	var pageTitle = "Current Sales";
-	document.title = WEBSITE_NAME + " - " + pageTitle;
-	var title = document.getElementById("header");
-	var paragraph = document.getElementById("main-body-paragraph");
-	
-	title.innerHTML = pageTitle;
-	paragraph.innerHTML = "Here's our current sales prices!<br>(This could be an image, a PDF, or just a text menu.)";
+	hideAllPages();
+	document.title = WEBSITE_NAME + " - Current Sales";
+	var current_page = document.getElementById("page-current-sales");
+	current_page.removeAttribute("hidden"); 
 }
 
 function displayError() {
@@ -82,25 +78,25 @@ function displayError() {
 }
 
 function checkUrlAndLoadCorrespondingDisplay() {
-	if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html#home" || window.location.href == "https://barrieradiator.github.io/#home" || window.location.href == "domain.com#home") {
+	if(window.location.href == INDEX_HTML_PATH + "#home" || window.location.href == "https://barrieradiator.github.io/#home" || window.location.href == "domain.com#home") {
 		displayHome();
 	}
-	else if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html" || window.location.href == "https://barrieradiator.github.io/" || window.location.href == "domain.com") {
+	else if(window.location.href == INDEX_HTML_PATH || window.location.href == "https://barrieradiator.github.io/" || window.location.href == "domain.com") {
 		displayIndex();
 	}
-	else if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html#about-us" || window.location.href == "https://barrieradiator.github.io/#about-us" || window.location.href == "domain.com#about-us") {
+	else if(window.location.href == INDEX_HTML_PATH + "#about-us" || window.location.href == "https://barrieradiator.github.io/#about-us" || window.location.href == "domain.com#about-us") {
 		displayAboutUs();
 	}
-	else if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html#contact-us" || window.location.href == "https://barrieradiator.github.io/#contact-us" || window.location.href == "domain.com#contact-us") {
+	else if(window.location.href == INDEX_HTML_PATH + "#contact-us" || window.location.href == "https://barrieradiator.github.io/#contact-us" || window.location.href == "domain.com#contact-us") {
 		displayContactUs();
 	}
-	else if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html#find-us" || window.location.href == "https://barrieradiator.github.io/#find-us" || window.location.href == "domain.com#find-us") {
+	else if(window.location.href == INDEX_HTML_PATH + "#find-us" || window.location.href == "https://barrieradiator.github.io/#find-us" || window.location.href == "domain.com#find-us") {
 		displayFindUs();
 	}
-	else if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html#services" || window.location.href == "https://barrieradiator.github.io/#services" || window.location.href == "domain.com#services") {
+	else if(window.location.href == INDEX_HTML_PATH + "#services" || window.location.href == "https://barrieradiator.github.io/#services" || window.location.href == "domain.com#services") {
 		displayServices();
 	}
-	else if(window.location.href == "file:///D:/Dev/Paid%20Work/2022/05%20-%20May/Barrie%20Radiator/barrieradiator.github.io/index.html#current-sales" || window.location.href == "https://barrieradiator.github.io/#current-sales" || window.location.href == "domain.com#current-sales") {
+	else if(window.location.href == INDEX_HTML_PATH + "#current-sales" || window.location.href == "https://barrieradiator.github.io/#current-sales" || window.location.href == "domain.com#current-sales") {
 		displayCurrentSales();
 	}
 	else {
